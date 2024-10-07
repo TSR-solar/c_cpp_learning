29f7fa13f227a0dfc7b1a3d2d06e88a51587f511
solxr <629086480@qq.com>
2024-10-07 21:17:23 +0800

    g++编译器和c++第一节cout的添加

---
+++ c++/#01.cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"Hello World\n";
+    return 0;
+}
+/*
+    iostream是c++的头文件，还想用C的头文件的话就只能在前面加“c”（不用加.h）；
+    如stdio.h变成cstdio；
+
+    std是命名空间，包含大量基本函数。
+    不同命名空间的函数定义是不一样的。
+
+    cout就是printf，加上<<即可。
+*/
